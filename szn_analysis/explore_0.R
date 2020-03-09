05b0811ce12acdbe5ca3ce5ccaf225d30050dae7
Jake Whalen <tjw4ra@virginia.edu>
2020-03-08 23:53:21 -0400

    Initial commit for 2020 competition

---
+++ szn_analysis/explore_0.R
@@ -0,0 +1,61 @@
+#Packages----
+library(tidyverse)
+library(magrittr)
+
+#Read Data----
+data.folder <- file.path(getwd(), 'data')
+df.19 <- read.csv(file.path(data.folder, 'MEvents2019.csv'))
+df.plyr <- read.csv(file.path(data.folder, 'MPlayers.csv'))
+df.tms <- read.csv(file.path(data.folder, 'MDataFiles_Stage1', 'MTeams.csv'))
+
+#Individual vs Distributed Scoring----
+#Create a DF of player event counts
+df.plyr.evnt <-
+    df.19 %>%
+    group_by(DayNum, WTeamID, PlayerID = EventPlayerID,
+             TeamID = EventTeamID, EventType,
+             WFinalScore, LFinalScore) %>% 
+    count(.) %>% #gets the count of each event type by player
+    ungroup() %>% 
+    filter(PlayerID != 0) %>% #Remove events not associted to a player
+    spread(EventType, n, fill = 0)
+
+#DF of players contribution in pnts towards teams total
+df.plyr.cntr <-
+    df.plyr.evnt %>% 
+    select(DayNum, WTeamID, PlayerID, TeamID,
+           contains('made'), WFinalScore, LFinalScore) %>% 
+    mutate(PlayerScore = made1 * 1 + made2 * 2 + made3 * 3,
+           TeamScore = ifelse(WTeamID == TeamID, WFinalScore, LFinalScore),
+           Result = ifelse(WTeamID == TeamID, 'Win', 'Lose'),
+           Contribution = PlayerScore / TeamScore) %>% 
+    select(DayNum, PlayerID, TeamID, PlayerScore,
+           TeamScore, Result, Contribution) %>% 
+    mutate(contr.bin = cut(Contribution, breaks = seq(0, 1, .1)),
+           contr.bin.m = cut(Contribution,
+                             breaks = seq(0, 1, .1),
+                             labels = seq(0.05,  0.95, 0.1)))
+
+#Number of Players who Scored
+df.plyr.cntr %>% 
+    filter(PlayerScore > 0) %>% 
+    group_by(DayNum, TeamID, Result) %>% 
+    count() %>% 
+    ggplot() +
+    geom_bar(aes(x=n, fill=Result), position = 'dodge') +
+    labs(title = 'Number of Players Contributing to Scoring',
+         x = 'No. Players Scored', y = 'Games') +
+    theme_classic()
+
+
+
+
+df.plyr.cntr %>% 
+    group_by(DayNum, TeamID, Result, contr.bin.m) %>% 
+    count() %>% 
+    filter(!is.na(contr.bin.m)) %>% 
+    mutate(contr.weight = as.numeric(as.character(contr.bin.m)) * n) %>% 
+    group_by(DayNum, TeamID, Result) %>% 
+    summarise(contr.weight = sum(contr.weight)) %>% 
+    ggplot() +
+    geom_density(aes(x=contr.weight, fill=Result))
