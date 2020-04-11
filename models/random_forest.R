3a8becaa69a74e8b6dff9d215a625cc95334a652
Jake Whalen <tjw4ra@virginia.edu>
2020-04-11 11:26:20 -0400

    Calculations for regular season wins and losses

--- models/random_forest.R
+++ models/random_forest.R
@@ -18,9 +18,38 @@ tourney_seeds <- dfs$MNCAATourneySeeds %>% mutate(Seed = parse_number(Seed))
 #Add team seeds to the feature df
 tourney_games %<>% 
     left_join(.,
-              tourney_seeds %>%rename(T_1_ID = TeamID, T_1_Seed = Seed),
+              tourney_seeds %>% rename(T_1_ID = TeamID, T_1_Seed = Seed),
               by = c("Season", "T_1_ID")) %>% 
     left_join(.,
               tourney_seeds %>% rename(T_2_ID = TeamID, T_2_Seed = Seed),
+              by = c("Season", "T_2_ID"))
+
+#Add season winning % and avg pts for and against
+reg_szn_stats <-
+    left_join(dfs$MRegularSeasonCompactResults %>% 
+              group_by(Season, TeamID = WTeamID) %>% 
+              summarise(Wins = n(), #calculate wins/losses/pts when team wins
+                        PtsFor_W = sum(WScore),
+                        PtsAgn_W = sum(LScore)),
+          dfs$MRegularSeasonCompactResults %>% 
+              group_by(Season, TeamID = LTeamID) %>% 
+              summarise(Losses = n(), #calculate wins/losses/pts when team loses
+                        PtsFor_L = sum(LScore),
+                        PtsAgn_L = sum(WScore)),
+          by = c("Season", "TeamID")) %>% 
+    mutate(WinPct = Wins / (Wins + Losses),
+           AvgPtsFor = (PtsFor_W + PtsFor_L) / (Wins + Losses),
+           AvgPtsAgn = (PtsAgn_W + PtsAgn_L) / (Wins + Losses))
+
+tourney_games %<>% 
+    left_join(.,
+              reg_szn_stats %>% 
+                  select(T_1_ID = TeamID, T_1_WinPct = WinPct,
+                         T_1_AvgPtsFor = AvgPtsFor, T_1_AvgPtsAgn = AvgPtsAgn),
+              by = c("Season", "T_1_ID")) %>% 
+    left_join(.,
+              reg_szn_stats %>% 
+                  select(T_2_ID = TeamID, T_2_WinPct = WinPct,
+                         T_2_AvgPtsFor = AvgPtsFor, T_2_AvgPtsAgn = AvgPtsAgn),
               by = c("Season", "T_2_ID"))
 
